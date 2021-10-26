dabe59bcdb4852ec90652fa4b704ed5221280137
Amal Hajji <amal.hajji@cdbdx.biz>
2021-10-26 10:20:28 +0200

    feat: 🎸 add them provider

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
 import HomePage from "../home/HomePage";
+import theme from "../ds/theme";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <HomePage />
-    </>
+    </ThemeProvider>
   );
 }
