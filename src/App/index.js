f9c64c15defc46f6b2a02c249e37212bb67e42d7
Amal Hajji <amal.hajji@cdbdx.biz>
2021-10-26 13:59:40 +0200

    feat: 🎸 routing

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { homePath, myListPath, searchPath } from "../routes";
 
 import HomePage from "../home/HomePage";
 import SearchPage from "../search/SearchPage";
@@ -13,14 +14,14 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Switch>
-          <Route exact path="/">
+          <Route exact path={homePath}>
             <HomePage />
           </Route>
         </Switch>
-        <Route path="/search">
+        <Route path={searchPath}>
           <SearchPage />
         </Route>
-        <Route path="/my-list">
+        <Route path={myListPath}>
           <MyListPage />
         </Route>
       </ThemeProvider>
--- src/ds/BaseLayout/index.js
+++ src/ds/BaseLayout/index.js
@@ -1,6 +1,7 @@
 import { node } from "prop-types";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { HomeOutlined, SearchOutlined, ListOutlined } from "@material-ui/icons";
+import { homePath, myListPath, searchPath } from "../../routes";
 
 import MenuItem from "../MenuItem";
 import Separator from "../Separator";
@@ -11,17 +12,17 @@ export default function BaseLayout({ children }) {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">Mega Movies</Typography>
-          <MenuItem startIcon={<HomeOutlined />} to="/">
+          <MenuItem startIcon={<HomeOutlined />} to={homePath}>
             Acceuil
           </MenuItem>
-          <MenuItem startIcon={<SearchOutlined />} to="search">
+          <MenuItem startIcon={<SearchOutlined />} to={searchPath}>
             Recherche
           </MenuItem>
-          <MenuItem startIcon={<ListOutlined />} to="my-list">
+          <MenuItem startIcon={<ListOutlined />} to={myListPath}>
             Ma liste
           </MenuItem>
           <Separator />
-          <MenuItem>Login</MenuItem>
+          <MenuItem to="/">Login</MenuItem>
         </Toolbar>
       </AppBar>
       {children}
