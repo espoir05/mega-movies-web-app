6c2041aa8e99a62289b16ea2fc7df630dfc0ae78
Amal Hajji <amal.hajji@cdbdx.biz>
2021-11-02 09:03:57 +0100

    feat: 🎸 prepare HomePage to display movies list

--- src/App/index.js
+++ src/App/index.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { homePath, myListPath, searchPath } from "../routes";
 import { Provider as ReactProvider } from "react-redux";
+import { QueryClientProvider, QueryClient } from "react-query;";
 
 import HomePage from "../home/HomePage";
 import SearchPage from "../search/SearchPage";
@@ -11,26 +12,30 @@ import theme from "../ds/theme";
 import store from "./store";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <ReactProvider store={store}>
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Switch>
-              <Route exact path={homePath}>
-                <HomePage />
-              </Route>
-              <Route path={searchPath}>
-                <SearchPage />
-              </Route>
-              <Route path={myListPath}>
-                <MyListPage />
-              </Route>
-            </Switch>
-          </ThemeProvider>
-        </BrowserRouter>
-      </ReactProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactProvider store={store}>
+          <BrowserRouter>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Switch>
+                <Route exact path={homePath}>
+                  <HomePage />
+                </Route>
+                <Route path={searchPath}>
+                  <SearchPage />
+                </Route>
+                <Route path={myListPath}>
+                  <MyListPage />
+                </Route>
+              </Switch>
+            </ThemeProvider>
+          </BrowserRouter>
+        </ReactProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- src/home/HomePage/index.js
+++ src/home/HomePage/index.js
@@ -1,25 +1,27 @@
-import { Alert } from "@material-ui/lab";
 import { Grid } from "@material-ui/core";
+import { v4 as uuid } from "uuid";
 import React from "react";
 
 import BaseLayout from "../../ds/BaseLayout";
 import MovieCard from "../../ds/MovieCard";
-import { useMovies } from "../../hooks";
+
+const defaultMovies = new Array(20).fill(null).map(() => ({
+  id: uuid(),
+  title: "Titanic",
+  image: "https://api.lorem.space/image/movie?w=350&h=210",
+}));
 
 export default function HomePage() {
-  const { movies, hasMovies } = useMovies();
+  const [movies] = React.useState(defaultMovies);
+
   return (
     <BaseLayout>
       <Grid container spacing={2}>
-        {hasMovies ? (
-          movies.map((movie) => (
-            <Grid item key={movie.id}>
-              <MovieCard {...movie} />
-            </Grid>
-          ))
-        ) : (
-          <Alert security="info"> aucun movies </Alert>
-        )}
+        {movies.map((movie) => (
+          <Grid item key={movie.id}>
+            <MovieCard {...movie} />
+          </Grid>
+        ))}
       </Grid>
     </BaseLayout>
   );
